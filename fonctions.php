0271a41018ee5b8ed8fd67a1fe98a9e9f08d6b9a
DRISS BELAROUSSI <belarous@iut.univ-paris5.fr>
2017-11-10 15:30:50 +0100

    fonctions requetes

--- fonctions.php
+++ fonctions.php
@@ -14,27 +14,59 @@ echo "bonjour";
 		return false ;
 	}
 	
-	function redigerCompteRendu($tableau){
+	function redigerCompteRendu($idPatient, $idMedecin, $contenuCR){
+		$stmt = SPDO::getDB() ;
+		$req =  "insert into CompteRendu ( idPatient , idMedecin, contenuCompteRendu ) values (:idPatient , :idMedecin , :contenuCR) ; " ;
+		$stmt->prepare($req);
+				$stmt->bindValue(':idPatient', $idPatient);
+				$stmt->bindValue(':idMedecin', $idMedecin);
+				$stmt->bindValue(':contenuCR', $contenuCR);
+				$stmt->execute();																					   
 		return false ; 
 	}
 	
 	function getCompteRendu($idPatient, $idMedecin, $date){
-		
+		$stmt = SPDO::getDB() ;
+		$req = " select * from CompteRendu where idPatient = :idPatient and idMedecin = :idMedecin and date = :date ; " ;
+		$stmt->prepare($req);
+				$stmt->bindValue(':idPatient', $idPatient);
+				$stmt->bindValue(':idMedecin', $idMedecin);
+				$stmt->bindValue(':date', $date);
+				$stmt->execute();
 	}
 	
 	function supprimerCompteRendu($idCR){
+		$stmt = SPDO::getDB() ;
+		$req = "delete from CompteRendu where idCompteRendu = :idCR ; " ;
+		$stmt->prepare($req);
+				$stmt->bindValue(':idCR', $idCR);
+				$stmt->execute();
 		return false ;		
 	}
 	
 	function getRDV($idPatient, $idMedecin, $date ){
+		$stmt = SPDO::getDB() ;
+		$req = "select * from RDV where idPatient = :idPatient and idMedecin = :idMedecin and date = :date" ;		
+		$stmt->bindValue(':idPatient', $idPatient);
+				$stmt->bindValue(':idMedecin', $idMedecin);
+				$stmt->bindValue(':date', $date);
+				$stmt->execute();
 		return false ;
 	}
 	
 	function accepterRDV( $idRDV ){
+		$stmt = SPDO::getDB() ;
+		$req = " update RDV set etat = 'A' where idRDV = :idRDV ;" ; 
+		$stmt->bindValue(':idRDV', $idRDV);
+				$stmt->execute();
 		return false;
 	}
 	
 	function refuserRDV( $idRDV ){
+		$stmt = SPDO::getDB() ;
+		$req = " update RDV set etat = 'R' where idRDV = :idRDV ;" ; 
+		$stmt->bindValue(':idRDV', $idRDV);
+				$stmt->execute();
 		return false ; 
 	}
 echo "au revoir";
