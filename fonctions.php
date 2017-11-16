da3ff02a6df2ab7733a9da655e0cc3ab07999033
QUENTIN DESABLEAU <desablea@iut.univ-paris5.fr>
2017-11-16 10:09:42 +0100

    Ajout prise rendez-vous

--- fonctions.php
+++ fonctions.php
@@ -1,11 +1,7 @@
 <?php
 require_once 'SPDO.php' ;
-
-
-
     function connexion(){
     $msg = '' ; 
-
 	if (isset($_POST['login']) && isset($_POST['mdp']) && trim($_POST['login']) != '' && trim($_POST['mdp']) != '')
 	{
 		try
@@ -13,7 +9,6 @@ require_once 'SPDO.php' ;
 			$bd = spdo::getDB ();
 					$medecin = false ;
 					$patient = false ; 
-
 					$txt = 'SELECT nomMedecin, prenomMedecin FROM Medecin WHERE login = :login and mdp =:mdp' ;
 					$req = $bd->prepare($txt);
 					$req->bindValue(':login', $_POST['login']);
@@ -21,16 +16,13 @@ require_once 'SPDO.php' ;
 					$req->execute();
 					$res1 = $req->fetch(PDO::FETCH_ASSOC);
 					
-
 					$txt = 'SELECT nomPatient, prenomPatient FROM Patient WHERE login = :login and mdp =:mdp';
 					$req = $bd->prepare($txt);
 					$req->bindValue(':login', $_POST['login']);
 					$req->bindValue(':mdp', $_POST['mdp']);
 					$req->execute();
 					$res2 = $req->fetch(PDO::FETCH_ASSOC);
-
 		
-
 					if($res1){
 						$medecin = true ;
 					
@@ -39,7 +31,6 @@ require_once 'SPDO.php' ;
 						$_SESSION['categorie'] = 'medecin' ;
 						$_SESSION['login'] =  $_POST['login'];
 						$_SESSION['connecte'] = true;
-
 						$msg = "Connexion Medecin réussie - Bienvenue ". $_SESSION['prenom'] ;			
 						var_dump($_SESSION);
 					} elseif($res2){
@@ -65,137 +56,256 @@ require_once 'SPDO.php' ;
 	return $msg ; 
 }
 	
-	function deconnexion(){
-		session_unset() ; 
+	function deconnexion($login){
+		return false ; 
 	}
 	
-		function inscriptionPatient() {
-		
-		$bd = spdo::getDB ();
-		if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['datenaissance']) && isset($_POST['adresse']) && isset($_POST['ville']) && isset($_POST['tel']) && isset($_POST['mail']) && isset($_POST['cp'])){
-			
-			$nom = $_POST['nom'];
-			$prenom = $_POST['prenom'];
-			$dateNaissance = $_POST['datenaissance'];
-			$adresse = $_POST['adresse'];
-			$ville = $_POST['ville'];
-			$cp = $_POST['cp'];
-			$tel = $_POST['tel'];
-			$mail = $_POST['mail'];
-			$login = substr(strtolower($prenom), 0, 1).strtolower($nom);
-			$mdp = "azerty";
-		
-			$req = "INSERT INTO Patient (nomPatient, prenomPatient, dateNaissance, adresse, ville, cp, tel, mail, login, mdp) VALUES (:nom, :prenom, :datenaissance, :adresse, :ville, :cp, :tel, :mail, :login, :mdp);";
-			$stmt = $bd->prepare($req);
-			$stmt->bindValue(':nom', $nom);
-			$stmt->bindValue(':prenom', $prenom);
-			$stmt->bindValue(':datenaissance', $dateNaissance);
-			$stmt->bindValue(':adresse', $adresse);
-			$stmt->bindValue(':ville', $ville);
-			$stmt->bindValue(':cp', $cp);
-			$stmt->bindValue(':tel', $tel);
-			$stmt->bindValue('mail', $mail);
-			$stmt->bindValue(':login', $login);
-			$stmt->bindValue(':mdp', $mdp);
-			$res = $stmt->execute();
-			
-			if($res){
-				return true;
-			} else {
-				return false;
+	function inscriptionPatient() {
+		try
+		{
+			$bd = spdo::getDB ();
+			if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['datenaissance']) && isset($_POST['adresse']) && isset($_POST['ville']) && isset($_POST['tel']) && isset($_POST['mail']) && isset($_POST['cp']))
+			{
+				if(trim($_POST['nom']) != '' && trim($_POST['prenom']) != '' && trim($_POST['datenaissance']) != '' && trim($_POST['adresse']) != '' && trim($_POST['ville']) != '' && trim($_POST['tel']) != '' && trim($_POST['mail']) != '' && trim($_POST['cp'])) != ''
+				{
+					$nom = $_POST['nom'];
+					$prenom = $_POST['prenom'];
+					$dateNaissance = $_POST['datenaissance'];
+					$adresse = $_POST['adresse'];
+					$ville = $_POST['ville'];
+					$cp = $_POST['cp'];
+					$tel = $_POST['tel'];
+					$mail = $_POST['mail'];
+					$login = substr(strtolower($prenom), 0, 1).strtolower($nom);
+					$mdp = "azerty";
+					
+					$req = "INSERT INTO Patient (nomPatient, prenomPatient, dateNaissance, adresse, ville, cp, tel, mail, login, mdp) VALUES (:nom, :prenom, :datenaissance, :adresse, :ville, :cp, :login, :mdp);";
+					$stmt = $bd->prepare($req);
+					$stmt->bindValue(':nom', $nom);
+					$stmt->bindValue(':prenom', $prenom);
+					$stmt->bindValue(':datenaissance', $dateNaissance);
+					$stmt->bindValue(':adresse', $adresse);
+					$stmt->bindValue(':ville', $ville);
+					$stmt->bindValue(':cp', $cp);
+					$stmt->bindValue(':tel', $tel);
+					$stmt->bindValue('mail', $mail);
+					$stmt->bindValue(':login', $login);
+					$stmt->bindValue(':mdp', $mdp);
+					$res = $stmt->execute();
+					
+					if($res){
+						return true;
+					} else {
+						return false;
+					}
+				}
 			}
+		}
 		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
+	}
+	
+	function updateUser($login, $nomPatient, $prenomPatient, $adresse, $dateNaissance, $ville, $tel, $mail, $cp){
+		try
+		{
+			$bd = spdo::getDB ();
+			$txt ="UPDATE Patient SET nomPatient = :nomPatient, prenomPatient= :prenomPatient, dateNaissance= :dateNaissance, adresse= :adresse, ville= :ville, cp= :cp,  tel= :tel, mail= :mail WHERE login = :login";
+			$req = $bd->prepare($txt);
+			$req->bindValue(':nomPatient', $nomPatient);
+			$req->bindValue(':prenomPatient', $prenomPatient);
+			$req->bindValue(':dateNaissance', $dateNaissance);
+			$req->bindValue(':adresse', $adresse);
+			$req->bindValue(':ville', $ville);
+			$req->bindValue(':cp', $cp);
+			$req->bindValue(':login', $login);
+			$req->bindValue(':mail', $mail);
+			$req->bindValue(':tel', $tel);
+			$req->execute();	
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
+	}
+	
+	function updatePassword($mdp, $mdp2){
+		try
+		{
+			$bd = spdo::getDB ();
+			
+			$txt = "UPDATE Patient SET mdp= :nouvmdp WHERE login=:login";
+			$req = $bd->prepare($txt); 
+			$req->bindValue(':nouvmdp', $nouvmdp);
+			$req->bindValue(':login', $login);
+			$req->execute();
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
 		}
 	}
 	
 	function redigerCompteRendu($idPatient, $idMedecin, $contenuCR){
-		$stmt = SPDO::getDB() ;
-		$req =  "insert into CompteRendu ( idPatient , idMedecin, contenuCompteRendu ) values (:idPatient , :idMedecin , :contenuCR) ; " ;
-		$stmt->prepare($req);
+		try
+		{
+			$stmt = SPDO::getDB() ;
+			$req =  "insert into CompteRendu ( idPatient , idMedecin, contenuCompteRendu ) values (:idPatient , :idMedecin , :contenuCR) ; " ;
+			$stmt->prepare($req);
 				$stmt->bindValue(':idPatient', $idPatient);
 				$stmt->bindValue(':idMedecin', $idMedecin);
 				$stmt->bindValue(':contenuCR', $contenuCR);
 				$stmt->execute();																					   
-		return false ; 
+			return false ;
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
 	}
 	
 	function getCompteRendu($idPatient, $idMedecin, $date){
-		$stmt = SPDO::getDB() ;
-		$req = " select * from CompteRendu where idPatient = :idPatient and idMedecin = :idMedecin and date = :date ; " ;
-		$stmt->prepare($req);
+		try
+		{
+			$stmt = SPDO::getDB() ;
+			$req = " select * from CompteRendu where idPatient = :idPatient and idMedecin = :idMedecin and date = :date ; " ;
+			$stmt->prepare($req);
 				$stmt->bindValue(':idPatient', $idPatient);
 				$stmt->bindValue(':idMedecin', $idMedecin);
 				$stmt->bindValue(':date', $date);
 				$stmt->execute();
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
 	}
 	
 	function supprimerCompteRendu($idCR){
-		$stmt = SPDO::getDB() ;
-		$req = "delete from CompteRendu where idCompteRendu = :idCR ; " ;
-		$stmt->prepare($req);
+		try
+		{
+			$stmt = SPDO::getDB() ;
+			$req = "delete from CompteRendu where idCompteRendu = :idCR ; " ;
+			$stmt->prepare($req);
 				$stmt->bindValue(':idCR', $idCR);
 				$stmt->execute();
-		return false ;		
+			return false ;
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
+	}
+	
+	function prendreRDV()
+	{
+		try
+		{
+			$bd = SPDO::getDB();
+			if (isset($_POST['date']) && isset($_POST['nom']) && isset($_POST['prenom'])
+			{
+				if (trim($_POST['date'] && isset($_POST['nom'] && isset($_POST['prenom'])
+				{
+					$date = $_POST['date'];
+					
+					$txt = "select idMedecin from Medecin where nomMedecin = :nomMedecin and prenomMedecin = :prenomMedecin";
+					$req = $bd->prepare($txt);
+					$req->bindValue(':nomMedecin', $_POST['nom']);
+					$req->bindValue(':prenomMedecin', $_POST['prenom']);
+					$req->execute();
+					$res = $req->fetch(PDO::FETCH_ASSOC);
+					if ($res)
+					{
+						$idMedecin = $res['idMedecin'];
+						
+						$txt = "select idPatient from Patient where nomPatient = :nomPatient and prenomPatient = :prenomPatient";
+						$req = $bd->prepare($txt);
+						$req->bindValue(':nomPatient', $_SESSION['nom']);
+						$req->bindValue(':prenomPatient', $_SESSION['prenom']);
+						$req->execute();
+						$res = $req->fetch(PDO::FETCH_ASSOC);
+						$idPatient = $res['idPatient'];
+						
+						$txt = "insert into table RDV values (:date, :idPatient, :idMedecin, 'C'";
+						$req = $bd->prepare($txt);
+						$req->bindValue(':date', $date);
+						$req->bindValue(':idPatient', $idPatient);
+						$req->bindValue(':idMedecin', $idMedecin);
+						$req->execute();
+						return -1;
+					}
+					
+					else
+					{
+						return -2;
+					}
+				}
+			}
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
 	}
 	
 	function getRDV($idPatient, $idMedecin, $date ){
-		$stmt = SPDO::getDB() ;
-		$req = "select * from RDV where idPatient = :idPatient and idMedecin = :idMedecin and date = :date" ;		
-		$stmt->bindValue(':idPatient', $idPatient);
+		try
+		{
+			$stmt = SPDO::getDB() ;
+			$req = "select * from RDV where idPatient = :idPatient and idMedecin = :idMedecin and date = :date" ;		
+			$stmt->bindValue(':idPatient', $idPatient);
 				$stmt->bindValue(':idMedecin', $idMedecin);
 				$stmt->bindValue(':date', $date);
 				$stmt->execute();
-		return false ;
+			return false ;
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
 	}
 	
 	function accepterRDV( $idRDV ){
-		$stmt = SPDO::getDB() ;
-		$req = " update RDV set etat = 'A' where idRDV = :idRDV ;" ; 
-		$stmt->bindValue(':idRDV', $idRDV);
+		try
+		{
+			$stmt = SPDO::getDB() ;
+			$req = " update RDV set etat = 'A' where idRDV = :idRDV ;" ; 
+			$stmt->bindValue(':idRDV', $idRDV);
 				$stmt->execute();
-		return false;
+			return false;
+		}
+		
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
 	}
 	
 	function refuserRDV( $idRDV ){
-		$stmt = SPDO::getDB() ;
-		$req = " update RDV set etat = 'R' where idRDV = :idRDV ;" ; 
-		$stmt->bindValue(':idRDV', $idRDV);
+		try
+		{
+			$stmt = SPDO::getDB() ;
+			$req = " update RDV set etat = 'R' where idRDV = :idRDV ;" ; 
+			$stmt->bindValue(':idRDV', $idRDV);
 				$stmt->execute();
-		return false ; 
-	}
-
-
-	function updateUser($login, $nomPatient, $prenomPatient, $adresse, $dateNaissance, $ville, $tel, $mail, $cp){
-		$bd = spdo::getDB ();
-		$txt ="UPDATE Patient SET nomPatient = :nomPatient, prenomPatient= :prenomPatient, dateNaissance= :dateNaissance, adresse= :adresse, ville= :ville, cp= :cp,  tel= :tel, mail= :mail WHERE login = :login";
-
-
-		$req = $bd->prepare($txt);
-
-		$req->bindValue(':nomPatient', $nomPatient);
-		$req->bindValue(':prenomPatient', $prenomPatient);
-		$req->bindValue(':dateNaissance', $dateNaissance);
-		$req->bindValue(':adresse', $adresse);
-		$req->bindValue(':ville', $ville);
-		$req->bindValue(':cp', $cp);
-		$req->bindValue(':login', $login);
-		$req->bindValue(':mail', $mail);
-		$req->bindValue(':tel', $tel);
-		$req->execute();	
-	}
-
-	function updatePassword($mdp, $mdpAncien){
-		$bd = spdo::getDB ();
-
+			return false ;
+		}
 		
-				$txt = "UPDATE Patient SET mdp= :nouvmdp WHERE login=:login and mdp =:mdp";
-				$req = $bd->prepare($txt); 
-				$req->bindValue(':nouvmdp', $mdp);
-				$req->bindValue(':login', $_SESSION['login']);
-				$req->bindValue(':mdp', $mdpAncien);
-				$req->execute();
+		catch (PDOException $e)
+		{
+			die('<p> La connexion a échoué. Erreur[' . $e->getCode() . '] : ' . $e->getMessage() . '</p>');
+		}
+	}
 
-	}		
-
+echo "au revoir";
 	
 ?>
